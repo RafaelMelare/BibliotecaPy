c3b63850fa508e7df08a65404206e6191e8d5ee6
Davi-SO <80834681+Davi-SO@users.noreply.github.com>
2023-06-18 20:33:40 -0300

    Update GerenciadorDeUsuario.py

--- GerenciadorDeUsuario.py
+++ GerenciadorDeUsuario.py
@@ -3,9 +3,9 @@ class GerenciadorDeUsuario:
         self.usuarios = []
 
     def adicionar_usuario(self, usuario):
-        if usuario.obter_nome() != "":
+        if usuario.get_nome() != "":
             self.usuarios.append(usuario)
-            print(f"Usuário '{usuario.obter_nome()}' adicionado à biblioteca.")
+            print(f"Usuário '{usuario.get_nome()}' adicionado à biblioteca.")
 
         else:
             print("Nome de usuário inválido.")
@@ -13,14 +13,14 @@ class GerenciadorDeUsuario:
     def remover_usuario(self, usuario):
         if usuario in self.usuarios:
             self.usuarios.remove(usuario)
-            print(f"Usuário '{usuario.obter_nome()}' removido da biblioteca.")
+            print(f"Usuário '{usuario.get_nome()}' removido da biblioteca.")
         
         else:
-            print(f"Usuário '{usuario.obter_nome()}' não está registrado na biblioteca.")
+            print(f"Usuário '{usuario.get_nome()}' não está registrado na biblioteca.")
 
 
     def buscar_usuario(self, nome):
         for usuario in self.usuarios:
-            if usuario.obter_nome() == nome:
+            if usuario.get_nome() == nome:
                 return usuario
         return None
