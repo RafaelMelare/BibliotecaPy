0c3219058f3d970fb411ad0bd43e34d8f6d8b5cb
Davi-SO <80834681+Davi-SO@users.noreply.github.com>
2023-06-18 20:22:26 -0300

    Update GerenciadorDeLivro.py

--- GerenciadorDeLivro.py
+++ GerenciadorDeLivro.py
@@ -5,19 +5,19 @@ class GerenciadorDeLivro:
     def adicionar_livro(self, livro):
         self.livros.append(livro)
         
-        print(f"Livro '{livro.obter_titulo()}' adicionado à biblioteca.")
+        print(f"Livro '{livro.get_titulo()}' adicionado à biblioteca.")
             
     def remover_livro(self, livro):
         if livro in self.livros:
             self.livros.remove(livro)
-            print(f"Livro '{livro.obter_titulo()}' removido da biblioteca.")
+            print(f"Livro '{livro.get_titulo()}' removido da biblioteca.")
         
         else:
-            print(f"Livro '{livro.obter_titulo()}' não está na biblioteca.")
+            print(f"Livro '{livro.get_titulo()}' não está na biblioteca.")
             
     def buscar_livro(self, titulo):
         for livro in self.livros:
-            if livro.obter_titulo() == titulo:
+            if livro.get_titulo() == titulo:
                 return livro    
 
         return None
